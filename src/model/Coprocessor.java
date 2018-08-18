77e451554fbfe957390d8338aea27d9d4b25958d
cristianm123 <crisfemoar2011@hotmail.com>
2018-08-17 20:51:19 -0500

    Generator ordered

--- src/model/Coprocessor.java
+++ src/model/Coprocessor.java
@@ -2,4 +2,28 @@ package model;
 
 public class Coprocessor {
 
+	private Generator generator;
+	private Sorter sorter;
+	
+	public Coprocessor()
+	{
+		generator = null;
+		sorter = null;
+	}
+
+	public Generator getGenerator() {
+		return generator;
+	}
+
+	public Sorter getSorter() {
+		return sorter;
+	}
+
+	public void setGenerator(Generator generator) {
+		this.generator = generator;
+	}
+
+	public void setSorter(Sorter sorter) {
+		this.sorter = sorter;
+	}
 }
--- src/model/Generator.java
+++ src/model/Generator.java
@@ -2,4 +2,29 @@ package model;
 
 public class Generator {
 
+	public Generator(int nElements, int floor, int roof, boolean repeated) {
+		this.nElements = nElements;
+		this.floor = floor;
+		this.roof = roof;
+		this.repeated = repeated;
+	}
+	private int nElements; 			// # of elements
+	private int floor; 				// Interval's beginning 
+	private int roof; 				// Interval's end
+	private boolean repeated;		// if the numbers can be repeated
+	
+	public int[] alreadyOrdered()
+	{
+		int jumpFactor = (roof-floor)/nElements;
+		int[] array = new int[nElements];
+		int sum = (int)(Math.random()*jumpFactor);
+		array[0] = sum;
+		System.out.println(array[0]);
+		for (int i = 1; i < array.length; i++) {
+			array[i] = array[i-1]+sum;
+			sum = (int)(Math.random()*jumpFactor);
+			System.out.println(array[i]);
+		}
+		return array;
+	}
 }
